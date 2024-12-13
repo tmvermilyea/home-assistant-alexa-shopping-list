ae04953e05c3e569b63c9b673a41c84df8ff0ce2
tmvermilyea <45149910+tmvermilyea@users.noreply.github.com>
2024-12-13 18:32:34 +0000

    Add exception catch for missing "Remember Me" button.

--- server/alexa.py
+++ server/alexa.py
@@ -158,7 +158,10 @@ class AlexaShoppingList:
 
     def _handle_login_password_page(self):
         self.driver.find_element(By.ID, 'ap_password').send_keys(self.password)
-        self.driver.find_element(By.NAME, 'rememberMe').click()
+        try:
+            self.driver.find_element(By.NAME, 'rememberMe').click()
+        except:
+            pass
         self._login_submit_button()
 
 
